b67f39391fb02538ed226e17a20518c37f62c53f
Michael Ilyin <milyin@gmail.com>
2022-11-17 14:53:32 +0000

    removed default constructor from Owned

--- include/zenohcpp_base.h
+++ include/zenohcpp_base.h
@@ -6,6 +6,7 @@ namespace zenoh {
 
 template<typename ZC_OWNED_TYPE> class Owned {
 public:
+    Owned() = delete;
     Owned& operator=(const Owned& v) = delete;
     Owned(const Owned& v) = delete;
     Owned(ZC_OWNED_TYPE&& v) : _0(v) { v = {}; }
@@ -14,17 +15,20 @@ public:
     ZC_OWNED_TYPE take() { auto r = _0; _0 = {}; return r; }
     bool check() { return z_check(_0); }
 protected:
-    Owned() : _0({}) {};
     ZC_OWNED_TYPE _0;
 };
 
 template<typename ZC_CLOSURE_TYPE, typename ZC_CLOSURE_CALL_PARAM, typename ZCPP_CLOSURE_CALL_PARAM> class Closure : public Owned<ZC_CLOSURE_TYPE> {
 public:
     using Owned<ZC_CLOSURE_TYPE>::Owned;
-    template<typename LAMBDA> Closure(LAMBDA&& lambda) {
-        this->_0.context = new LAMBDA(std::move(lambda));
-        this->_0.drop = [](void* ctx){delete static_cast<LAMBDA*>(ctx);};
-        this->_0.call = [](ZC_CLOSURE_CALL_PARAM* pvalue, void* ctx){ static_cast<LAMBDA*>(ctx)->operator()(ZCPP_CLOSURE_CALL_PARAM(std::move(*pvalue))); };
+    template<typename LAMBDA> Closure(LAMBDA&& lambda) : Owned<ZC_CLOSURE_TYPE>(wrap_lambda_to_closure<LAMBDA>(std::move(lambda))) {}
+private:
+    template<typename LAMBDA> ZC_CLOSURE_TYPE wrap_lambda_to_closure(LAMBDA&& lambda) {
+        return {
+            context: new LAMBDA(std::move(lambda)),
+            call: [](ZC_CLOSURE_CALL_PARAM* pvalue, void* ctx){ static_cast<LAMBDA*>(ctx)->operator()(ZCPP_CLOSURE_CALL_PARAM(std::move(*pvalue))); },
+            drop: [](void* ctx){delete static_cast<LAMBDA*>(ctx);},
+        };
     }
 };
 
--- include/zenohcpp_objects.h
+++ include/zenohcpp_objects.h
@@ -19,14 +19,14 @@ private:
 class KeyExpr : Owned<::z_owned_keyexpr_t> {
 public:
     using Owned::Owned;
-    explicit KeyExpr(const char* name) { _0 = z_keyexpr_new(name); }
+    explicit KeyExpr(const char* name) : Owned(z_keyexpr_new(name)) {}
     operator KeyExprView() const { return KeyExprView(z_keyexpr_loan(&_0)); }
 };
 
 class Config : public Owned<::z_owned_config_t> {
 public:
     using Owned::Owned;
-    Config() { _0 = z_config_default(); }
+    Config() : Owned(z_config_default()) {}
     bool insert_json(const char* key, const char* value) 
         { return zc_config_insert_json(z_config_loan(&_0), key, value) == 0; }
 };
@@ -42,12 +42,14 @@ typedef Closure<::z_owned_closure_reply_t, ::z_owned_reply_t, Reply> ClosureRepl
 class Session : public Owned<::z_owned_session_t> {
 public:
     using Owned::Owned;
-    explicit Session(Config&& v) { 
-        auto config = v.take();
-        _0 = z_open(z_move(config));
-    }
+    explicit Session(Config&& v) : Owned(_z_open(std::move(v))) {} 
     bool get(KeyExprView keyexpr, const char* parameters, ClosureReply&& callback, const GetOptions& options) 
         { auto c = callback.take(); return z_get(z_session_loan(&_0), keyexpr, parameters, &c, &options); }
+private:
+    static ::z_owned_session_t _z_open(Config&& v) {
+        auto config = v.take();
+        return ::z_open(z_move(config));
+    };
 };
 
 
