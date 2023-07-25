589aa1fea3c0dd91d33319a7b3a99404f66be731
Michael Ilyin <milyin@gmail.com>
2023-07-25 18:04:10 +0200

    dosc initial

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+#
+# Copyright (c) 2023 ZettaScale Technology
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+# which is available at https://www.apache.org/licenses/LICENSE-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+#
+# Contributors:
+#   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+#
+
+# Configuration file for the Sphinx documentation builder.
+from clang.cindex import Config
+
+# -- Project information -----------------------------------------------------
+project = 'zenoh-cpp'
+copyright = '2017, 2023 ZettaScale Technology'
+author = 'ZettaScale Zenoh team'
+release = '0.10.0.0'
+
+# -- General configuration ---------------------------------------------------
+master_doc = 'index'
+extensions = ['sphinx_c_autodoc', 'sphinx_c_autodoc.napoleon']
+language = 'c'
+c_autodoc_roots = ['../include']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+
+# ----------------------------------------------------------------------------
+Config.set_library_file('/usr/lib/llvm-6.0/lib/libclang.so.1')
---
+++ docs/index.rst
@@ -0,0 +1,27 @@
+..
+.. Copyright (c) 2023 ZettaScale Technology
+..
+.. This program and the accompanying materials are made available under the
+.. terms of the Eclipse Public License 2.0 which is available at
+.. http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+.. which is available at https://www.apache.org/licenses/LICENSE-2.0.
+..
+.. SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+..
+.. Contributors:
+..   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+..
+
+*********
+zenoh-cpp
+*********
+
+The *zenoh-cpp* library provides a C++ client API for the zenoh protocol.
+
+An introduction to zenoh and its concepts is available on `zenoh.io <https://zenoh.io>`_.
+
+.. toctree::
+    :maxdepth: 10
+
+    examples
+    api
