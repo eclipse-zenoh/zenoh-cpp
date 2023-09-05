3142797495a3e1a4f5f6d39f34a2275a2e0c984b
Michael Ilyin <milyin@gmail.com>
2023-09-05 12:15:53 +0200

    compile error after merge fix

--- examples/universal/z_sub.cxx
+++ examples/universal/z_sub.cxx
@@ -26,74 +26,72 @@ using namespace zenoh;
 const char *kind_to_str(z_sample_kind_t kind);
 
 void data_handler(const Sample &sample) {
-    void data_handler(const Sample &sample) {
-        std::cout << ">> [Subscriber] Received " << kind_to_str(sample.get_kind()) << " ('"
-                  << sample.get_keyexpr().as_string_view() << "' : '" << sample.get_payload().as_string_view()
-                  << "')\n";
-    }
+    std::cout << ">> [Subscriber] Received " << kind_to_str(sample.get_kind()) << " ('"
+              << sample.get_keyexpr().as_string_view() << "' : '" << sample.get_payload().as_string_view() << "')\n";
+}
 
-    int _main(int argc, char **argv) {
-        const char *expr = "demo/example/**";
-        const char *locator = nullptr;
+int _main(int argc, char **argv) {
+    const char *expr = "demo/example/**";
+    const char *locator = nullptr;
 
-        if (argc > 1) expr = argv[1];
-        if (argc > 2) locator = argv[2];
+    if (argc > 1) expr = argv[1];
+    if (argc > 2) locator = argv[2];
 
-        Config config;
-        if (locator) {
+    Config config;
+    if (locator) {
 #ifdef ZENOHCXX_ZENOHC
-            auto locator_json_str_list = std::string("[\"") + locator + "\"]";
-            if (!config.insert_json(Z_CONFIG_CONNECT_KEY, locator_json_str_list.c_str()))
+        auto locator_json_str_list = std::string("[\"") + locator + "\"]";
+        if (!config.insert_json(Z_CONFIG_CONNECT_KEY, locator_json_str_list.c_str()))
 #elif ZENOHCXX_ZENOHPICO
-            if (!config.insert(Z_CONFIG_PEER_KEY, locator))
+        if (!config.insert(Z_CONFIG_PEER_KEY, locator))
 #else
 #error "Unknown zenoh backend"
 #endif
-            {
-                std::cout << "Invalid locator: " << locator << std::endl;
-                std::cout << "Expected value in format: tcp/192.168.64.3:7447" << std::endl;
-                exit(-1);
-            }
+        {
+            std::cout << "Invalid locator: " << locator << std::endl;
+            std::cout << "Expected value in format: tcp/192.168.64.3:7447" << std::endl;
+            exit(-1);
         }
+    }
 
-        KeyExprView keyexpr(expr);
+    KeyExprView keyexpr(expr);
 
-        std::cout << "Opening session..." << std::endl;
-        auto session = expect<Session>(open(std::move(config)));
+    std::cout << "Opening session..." << std::endl;
+    auto session = expect<Session>(open(std::move(config)));
 
-        std::cout << "Declaring Subscriber on '" << keyexpr.as_string_view() << "'..." << std::endl;
-        auto subscriber = expect<Subscriber>(session.declare_subscriber(keyexpr, data_handler));
+    std::cout << "Declaring Subscriber on '" << keyexpr.as_string_view() << "'..." << std::endl;
+    auto subscriber = expect<Subscriber>(session.declare_subscriber(keyexpr, data_handler));
 #ifdef ZENOHCXX_ZENOHC
-        std::cout << "Subscriber on '" << subscriber.get_keyexpr().as_string_view() << "' declared" << std::endl;
+    std::cout << "Subscriber on '" << subscriber.get_keyexpr().as_string_view() << "' declared" << std::endl;
 #endif
 
-        printf("Enter 'q' to quit...\n");
-        char c = 0;
-        while (c != 'q') {
-            c = getchar();
-            if (c == -1) {
-                sleep(1);
-            }
-        }
-
-        return 0;
-    }
-
-    const char *kind_to_str(z_sample_kind_t kind) {
-        switch (kind) {
-            case Z_SAMPLE_KIND_PUT:
-                return "PUT";
-            case Z_SAMPLE_KIND_DELETE:
-                return "DELETE";
-            default:
-                return "UNKNOWN";
+    printf("Enter 'q' to quit...\n");
+    char c = 0;
+    while (c != 'q') {
+        c = getchar();
+        if (c == -1) {
+            sleep(1);
         }
     }
 
-    int main(int argc, char **argv) {
-        try {
-            _main(argc, argv);
-        } catch (ErrorMessage e) {
-            std::cout << "Received an error :" << e.as_string_view() << "\n";
-        }
+    return 0;
+}
+
+const char *kind_to_str(z_sample_kind_t kind) {
+    switch (kind) {
+        case Z_SAMPLE_KIND_PUT:
+            return "PUT";
+        case Z_SAMPLE_KIND_DELETE:
+            return "DELETE";
+        default:
+            return "UNKNOWN";
     }
+}
+
+int main(int argc, char **argv) {
+    try {
+        _main(argc, argv);
+    } catch (ErrorMessage e) {
+        std::cout << "Received an error :" << e.as_string_view() << "\n";
+    }
+}
