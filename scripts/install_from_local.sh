a1fbb29a099d642d2fb7f952f2e11c8b8149f603
Denis Biryukov <denis.biryukov@zettascale.tech>
2024-07-22 11:08:53 +0200

    add install script for installing zenoh-c/pico from local folders

---
+++ scripts/install_from_local.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ "$#" -eq 0 ]; then
+    echo "Usage: install_from_local INSTALL_PATH"
+    exit
+fi
+
+mkdir -p $1
+absolute_install_location=$(cd $1; pwd)
+#build zenoh-c
+bash $SCRIPT_DIR/install_local.sh $SCRIPT_DIR/../../zenoh-c $absolute_install_location -DZENOHC_BUILD_WITH_UNSTABLE_API=TRUE -DZENOHC_BUILD_WITH_SHARED_MEMORY=TRUE
+#build zenoh-pico
+bash $SCRIPT_DIR/install_local.sh $SCRIPT_DIR/../../zenoh-pico $absolute_install_location
+
+rm -rf ./build
+mkdir ./build
+cd ./build
+cmake .. --install-prefix "$absolute_install_location"
+make
+make install
