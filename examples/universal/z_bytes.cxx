ee0f296ed156bce7f97af3752d8458a81b53645a
yellowhatter <bannov.dy@gmail.com>
2024-09-19 15:43:32 +0300

    clang format

--- examples/universal/z_bytes.cxx
+++ examples/universal/z_bytes.cxx
@@ -100,7 +100,7 @@ int _main(int argc, char** argv) {
     {
         const std::vector<uint8_t> input = {1, 2, 3, 4};
         const auto payload = Bytes::serialize(input);
-        
+
         auto idx = 0;
         auto it = payload.slice_iter();
         for (auto elem = it.next(); elem.has_value(); elem = it.next()) {
