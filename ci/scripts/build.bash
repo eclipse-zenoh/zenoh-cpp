3970a0486b27bcaa1a207a5be5d4373e49ad0334
Diogo Matsubara <diogo.matsubara@zettascale.tech>
2024-10-31 10:53:25 +0100

    Add release debian workflow (#271)
    * set deb and rpm architecture to any
    
    * fix: add job to publish debian packages
    
    * fix: update package names
    
    don't include toplevel dir in archives
    
    * fix: align cmake deb/rpm with zenoh-c
    
    * fix: pass DEBARCH in build script
    
    Avoids the debian package having an invalid debian architecture
    
    * fix: update build.bash to use correct zip name
    
    * fix: remove uneeded changes
    
    * fix: don't set debarch
    
    ---------
    
    Co-authored-by: Denis Biryukov <denis.biryukov@zettascale.tech>

--- ci/scripts/build.bash
+++ ci/scripts/build.bash
@@ -11,13 +11,14 @@ readonly out=$GITHUB_WORKSPACE
 # readonly repo_name=${repo#*/}
 # temporary fix - make manual
 readonly repo_name=zenohcpp
-readonly archive_lib=$out/$repo_name-$version.zip
-readonly archive_deb=$out/$repo_name-$version-deb-pkgs.zip
-readonly archive_rpm=$out/$repo_name-$version-rpm-pkgs.zip
+readonly archive_lib=$out/$repo_name-$version-standalone.zip
+readonly archive_deb=$out/$repo_name-$version-debian.zip
+readonly archive_rpm=$out/$repo_name-$version-rpm.zip
 
 # Make packages into build/packages
 mkdir -p build
 cd build
+# For now we only build for x86_64
 cmake -DZENOHCXX_ZENOHC=OFF ..
 cpack
 cpack -G DEB
@@ -25,7 +26,7 @@ cpack -G RPM
 ls -R
 
 cd "$GITHUB_WORKSPACE"
-mv "build/packages/$repo_name-$version.zip" "$archive_lib"
+mv "$(readlink -f build/packages/*.zip)" "$archive_lib"
 zip --verbose --junk-paths "$archive_deb" build/packages/*.deb
 zip --verbose --junk-paths "$archive_rpm" build/packages/*.rpm
 
