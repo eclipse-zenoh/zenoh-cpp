e582613dbc97677a2b9f150fadcd30aff639bf15
Michael Ilyin <milyin@gmail.com>
2024-11-19 15:34:05 +0000

    clang-format

--- include/zenoh/api/session.hxx
+++ include/zenoh/api/session.hxx
@@ -40,7 +40,7 @@
 namespace zenoh {
 /// A Zenoh session.
 class Session : public Owned<::z_owned_session_t> {
-    Session(zenoh::detail::null_object_t) : Owned(nullptr) {};
+    Session(zenoh::detail::null_object_t) : Owned(nullptr){};
 
    public:
     /// @brief Options to be passed when opening a ``Session``.
