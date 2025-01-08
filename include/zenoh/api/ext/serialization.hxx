8efbd20edbc790dbfa1a89252ebdcddd58117893
Yuyuan Yuan <az6980522@gmail.com>
2025-01-08 17:26:21 +0800

    chore: apply the suggestion
    Co-authored-by: DenisBiryukov91 <155981813+DenisBiryukov91@users.noreply.github.com>

--- include/zenoh/api/ext/serialization.hxx
+++ include/zenoh/api/ext/serialization.hxx
@@ -278,7 +278,7 @@ inline bool __zenoh_deserialize_with_deserializer(zenoh::ext::Deserializer& dese
     __ZENOH_RESULT_CHECK(::ze_deserializer_deserialize_string(interop::as_copyable_c_ptr(deserializer), &s), err,
                          "Deserialization failure");
     value = std::string(::z_string_data(::z_loan(s)), ::z_string_len(::z_loan(s)));
-    z_drop(z_move(s));
+    ::z_drop(::z_move(s));
     return err == nullptr || *err == Z_OK;
 }
 
