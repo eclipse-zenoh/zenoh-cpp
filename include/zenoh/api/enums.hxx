30cea3cd9e310241fc9f50fe131c0f3e5bfa3477
Joseph Perez <joperez@hotmail.fr>
2025-07-17 11:27:47 +0200

    fix doc

--- include/zenoh/api/enums.hxx
+++ include/zenoh/api/enums.hxx
@@ -57,16 +57,16 @@ typedef ::z_reliability_t Reliability;
 
 ///  Congestion control values.
 ///
-///   Values:
+///  Values:
 ///    - **Z_CONGESTION_CONTROL_BLOCK**: Defines congestion control as "block". Messages are not dropped in case of
 ///       congestion control
 ///    - **Z_CONGESTION_CONTROL_DROP**: Defines congestion control as "drop". Messages are dropped in case of
 ///      congestion control
 ///    - **Z_CONGESTION_CONTROL_BLOCK_FIRST**: Defines congestion control as "block first". Messages except the first
 ///      one are dropped in case of congestion control
-/// @warning **Z_CONGESTION_CONTROL_BLOCK_FIRST** has been marked as unstable: it works as advertised, but it may be changed in a future
+///  @warning **Z_CONGESTION_CONTROL_BLOCK_FIRST** has been marked as unstable: it works as advertised, but it may be changed in a future
 ///      release.
-/// @note **Z_CONGESTION_CONTROL_BLOCK_FIRST** is Zenoh-c only.
+///  @note **Z_CONGESTION_CONTROL_BLOCK_FIRST** is Zenoh-c only.
 ///
 typedef ::z_congestion_control_t CongestionControl;
 
